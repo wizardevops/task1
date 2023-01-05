beaf2c81ac2446f58ed9abbe100c5c440055481c
Alexander Malyshko <wizardevops@gmail.com>
2023-01-05 14:25:49 +0300

    task1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Kvitos ebet sobak</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><p>;
+echo "Made by <font color=blue>Alexander Malyshko";
+
+?>
